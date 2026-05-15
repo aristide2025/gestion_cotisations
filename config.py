a3451c940552615b341ffec5158aa6caf05b9262
Aristide2025 <dorineateba29@gmail.com>
2026-05-15 02:32:21 +0000

    Config connexion base de données Render

--- config.py
+++ config.py
@@ -1,14 +1,22 @@
 import psycopg2
+import os
 
 def get_connection():
-    conn = psycopg2.connect(
-        host     = "localhost",
-        database = "gestion_cotisations",
-        user     = "postgres",
-        password = "aristide08"  # ← remplacez par votre mot de passe PostgreSQL
-    )
+    database_url = os.environ.get('DATABASE_URL')
+    
+    if database_url:
+        # On est sur Render
+        conn = psycopg2.connect(database_url, sslmode='require')
+    else:
+        # On est en local
+        conn = psycopg2.connect(
+            host     = "localhost",
+            database = "gestion_cotisations",
+            user     = "postgres",
+            password = "votre_mot_de_passe"
+        )
     return conn
 
-    # Identifiants de connexion
-ADMIN_USERNAME = "Aristide"
-ADMIN_PASSWORD = "Bertin08"  # ← changez par votre mot de passe
+# Identifiants de connexion
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin123"
